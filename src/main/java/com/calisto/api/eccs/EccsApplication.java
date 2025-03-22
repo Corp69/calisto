3bd552388e63e237fbc20195a3b7bc9d5a1c7ad7
AzureAD\ElizandroCarballoCas <elizandro.carballo@macropay.mx>
2025-03-21 18:21:27 -0600

    primer commit

---
+++ src/main/java/com/calisto/api/eccs/EccsApplication.java
@@ -0,0 +1,13 @@
+package com.calisto.api.eccs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccsApplication.class, args);
+	}
+
+}
